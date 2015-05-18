f61629de6f682175c6bad7de528f120ad91216a2
aleksanderzwd <aleksander.zwd@gmail.com>
2015-05-18 14:17:43 +0200

    Correction of Plot4 - remove a border around the legend

--- plot4.R
+++ plot4.R
@@ -70,6 +70,7 @@ with(householdPowerConsumptionDataTable, {
         lines(DateTime, Sub_metering_3, col = "blue")
         legend("topright", lty=1, lwd=1,
                col=c("black", "red", "blue"), 
+               bty="n",
                legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
         # the plot number 3
